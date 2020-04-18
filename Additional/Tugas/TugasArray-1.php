15d2568160f0a9590389910cdd253ba3eafe9873
rifqi <mrfqbanna@gmail.com>
2020-04-18 21:59:28 +0700

    merubah nama tugas

---
+++ Additional/Tugas/TugasArray-1.php
@@ -0,0 +1,222 @@
+<?php
+
+$laptop = [
+    [
+        'jenislaptop' => 'GL65 9SDK',
+        'merklaptop' => 'MSI',
+        'spesifikasi' => 'Intel® Core™ i7 Processor',
+        'os' => [
+            'q1' => 'WINDOWS 10 PRO',
+            'q2' => 'WINDOWS 10 HOME'
+        ],
+        'display' => [
+            'p1' => '15.6" FHD (1920x1080), IPS-Level',
+            'p2' => '15.6" FHD (1920x1080), 120Hz, IPS-Level'
+        ],
+        'chipset' => 'Intel® HM370r',
+        'memory' => [
+            'p1' => 'DDR4-2666',
+            'p2' => '2 Slots',
+            'p3' => 'Max 64GB'
+        ],
+        'harga' => 'Rp 19.499.000',
+        'rating' => 'good',
+        'telpon' => [
+            'p1' => '(08129879)',
+            'p2' => '(09128738)'
+        ],
+        'email' => 'msilaptop@gmail.com',
+        'website' => 'https://msi.com',
+        'logo' => 'msi.png'
+    ],
+    [
+        'jenislaptop' => 'ROG Zephyrus S GX701',
+        'merklaptop' => 'ASUS',
+        'spesifikasi' => 'Intel® Core™ i7 Processor',
+        'os' => [
+            'q1' => 'WINDOWS 10 PRO',
+            'q2' => 'WINDOWS 10 HOME'
+        ],
+        'display' => [
+            'p1' => '17.0" FHD (1920x1080), IPS-Level',
+            'p2' => '17.0" FHD (1920x1080), 120Hz, IPS-Level'
+        ],
+        'chipset' => 'Intel® HM370r',
+        'memory' => [
+            'p1' => 'DDR4-2666',
+            'p2' => '2 Slots',
+            'p3' => 'Max 64GB'
+        ],
+        'harga' => 'Rp 21.499.000',
+        'rating' => 'good',
+        'telpon' => [
+            'p1' => '(08986152)',
+            'p2' => '(08716391)'
+        ],
+        'email' => 'asuslaptop@gmail.com',
+        'website' => 'https://asus.com',
+        'logo' => 'Untitled-1.png'
+    ],
+    [
+        'jenislaptop' => 'Legion Y530',
+        'merklaptop' => 'LENOVO',
+        'spesifikasi' => 'Intel® Core™ i7 Processor',
+        'os' => [
+            'q1' => 'WINDOWS 10 PRO',
+            'q2' => 'WINDOWS 10 HOME'
+        ],
+        'display' => [
+            'p1' => '15.0" FHD (1920x1080), IPS-Level',
+            'p2' => '15.0" FHD (1920x1080), 120Hz, IPS-Level'
+        ],
+        'chipset' => 'Intel® HM370r',
+        'memory' => [
+            'p1' => 'DDR4-2666',
+            'p2' => '2 Slots',
+            'p3' => 'Max 64GB'
+        ],
+        'harga' => 'Rp 17.000.000',
+        'rating' => 'good',
+        'telpon' => [
+            'p1' => '(07625128)',
+            'p2' => '(08653821)'
+        ],
+        'email' => 'lenovolaptop@gmail.com',
+        'website' => 'https://lenovo.com',
+        'logo' => 'legion.png'
+    ],
+    [
+        'jenislaptop' => 'PREDATOR HELIOS 300',
+        'merklaptop' => 'ACER',
+        'spesifikasi' => 'Intel® Core™ i7 Processor',
+        'os' => [
+            'q1' => 'WINDOWS 10 PRO',
+            'q2' => 'WINDOWS 10 HOME'
+        ],
+        'display' => [
+            'p1' => '15.6" FHD (1920x1080), IPS-Level',
+            'p2' => '15.6" FHD (1920x1080), 120Hz, IPS-Level'
+        ],
+        'chipset' => 'Intel® HM370r',
+        'memory' => [
+            'p1' => 'DDR4-2666',
+            'p2' => '2 Slots',
+            'p3' => 'Max 64GB'
+        ],
+        'harga' => 'Rp 18.000.000',
+        'rating' => 'good',
+        'telpon' => [
+            'p1' => '(08129879)',
+            'p2' => '(09128738)'
+        ],
+        'email' => 'acerlaptop@gmail.com',
+        'website' => 'https://acer.com',
+        'logo' => 'predator.png'
+    ],
+    [
+        'jenislaptop' => 'RAZER BLADE PRO 17',
+        'merklaptop' => 'RAZER',
+        'spesifikasi' => 'Intel® Core™ i7 Processor',
+        'os' => [
+            'q1' => 'WINDOWS 10 PRO',
+            'q2' => 'WINDOWS 10 HOME'
+        ],
+        'display' => [
+            'p1' => '15.6" FHD (1920x1080), IPS-Level',
+            'p2' => '15.6" FHD (1920x1080), 120Hz, IPS-Level'
+        ],
+        'chipset' => 'Intel® HM370r',
+        'memory' => [
+            'p1' => 'DDR4-2666',
+            'p2' => '2 Slots',
+            'p3' => 'Max 64GB'
+        ],
+        'harga' => 'Rp 22.000.000',
+        'rating' => 'good',
+        'telpon' => [
+            'p1' => '(08129879)',
+            'p2' => '(09128738)'
+        ],
+        'email' => 'razerlaptop@gmail.com',
+        'website' => 'https://razer.com',
+        'logo' => 'razer.png'
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<link rel="stylesheet" href="./pindad.css">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LAPTOP GAMING</title>
+</head>
+
+
+<body>
+    <h1 style="text-align: center"> JENIS LAPTOP GAMING </h1>
+    <table border="3">
+        <tr>
+
+            <th> logo</th>
+            <th> Jenis Laptop</th>
+            <th> Merk Laptop</th>
+            <th> Spesifikasi</th>
+            <th> OS</th>
+            <th> Display</th>
+            <th> Chipset</th>
+            <th> Memory</th>
+            <th> Rating</th>
+            <th> Harga</th>
+            <th> Telepon</th>
+            <th> Email</th>
+            <th> Website</th>
+        </tr>
+
+        <?php foreach ($laptop as $lpt) : ?>
+            <tr>
+                <td>
+                    <img style="width: 120px " src="./images/<?= $lpt['logo'] ?>" />
+                </td>
+                <td style="text-align: center"><?= $lpt['jenislaptop'] ?></td>
+                <td style="text-align: center"><?= $lpt['merklaptop'] ?></td>
+                <td style="text-align: center"><?= $lpt['spesifikasi'] ?></td>
+                <td style="text-align: center">
+                    <?php foreach ($lpt['os'] as $os) : ?>
+                        <?= $os . "<br>"; ?>
+                    <?php endforeach ?>
+                </td>
+                <td style="text-align: center">
+                    <?php foreach ($lpt['display'] as $display) : ?>
+                        <?= $display . "<br>"; ?>
+                    <?php endforeach ?>
+                </td>
+                <td style="text-align: center"><?= $lpt['chipset'] ?></td>
+                <td style="text-align: center">
+                    <?php foreach ($lpt['memory'] as $memory) : ?>
+                        <?= $memory . "<br>"; ?>
+                    <?php endforeach ?>
+                </td>
+                <td style="text-align: center"><?= $lpt['rating'] ?></td>
+                <td style="text-align: center"><?= $lpt['harga'] ?></td>
+                <td>
+                    <?php foreach ($lpt['telpon'] as $telp) : ?>
+                        <?= $telp . "<br>"; ?>
+                    <?php endforeach ?>
+                </td>
+                <td style="text-align: center"><?= $lpt['email'] ?></td>
+                <td style="text-align: center"><?= $lpt['website'] ?></td>
+            <?php endforeach ?>
+            </tr>
+
+
+    </table>
+
+
+
+</body>
+
+</html>
