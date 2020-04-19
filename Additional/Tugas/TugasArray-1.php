032dcdfe3ba9eac76f26ce1cab1fb6223457d2d9
rifqi <mrfqbanna@gmail.com>
2020-04-19 13:18:52 +0700

    menambah gambar

--- Additional/Tugas/TugasArray-1.php
+++ Additional/Tugas/TugasArray-1.php
@@ -147,13 +147,13 @@ $laptop = [
 <!DOCTYPE html>
 <html lang="en">
 
-<link rel="stylesheet" href="./pindad.css">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LAPTOP GAMING</title>
+
     <link rel="stylesheet" href="./pindad.css">
+
 </head>
 
 
